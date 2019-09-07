b1f8d1f1e700eff729293ee52d522efa54e4f46c
Julio Vedovatto <juliovedovatto@gmail.com>
2019-09-06 21:18:17 -0300

    Conversão Repository de Autenticação de Objeto para classe

--- src/repository/Auth.ts
+++ src/repository/Auth.ts
@@ -2,10 +2,14 @@ import Repository from './Repository'
 
 const resource = '/auth'
 
-export default {
-  async login (login: string, senha: string) {
+export default class {
+  static async login (login: string, senha: string) {
     const { data } = await Repository.post(`${resource}/login`, { login, senha })
 
     return data
+  }
+
+  static async isLogged () {
+    await Repository.get(`${resource}/is-logged`)
   }
 }
