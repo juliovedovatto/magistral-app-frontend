6ace6cda0bdea6ddc71b1fd92cbb028552ae4d10
Julio Vedovatto <juliovedovatto@gmail.com>
2019-09-06 21:17:18 -0300

    Adicionado plugins de máscara de campos, manipulação de data e converter nova linha em <br>

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,31 @@ import Router from '@/routes'
 import Store from '@/store'
 import Vue from 'vue'
 import VueContentPlaceholders from 'vue-content-placeholders'
+import VueDate from '@/plugins/date'
+import VueMask from 'v-mask'
+import VueNl2Br from 'vue-nl2br'
 import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
+// PLUGINS
 Vue.use(Vuex)
 Vue.use(BootstrapVue)
 Vue.use(VueContentPlaceholders)
+Vue.use(VueMask)
+Vue.use(VueDate)
 
-new Vue({ router: Router, store: Store, render: (h) => h(App) }).$mount('#app')
+// GLOBAL COMPONENTS
+Vue.component('nl2br', VueNl2Br)
+
+new Vue({
+  router: Router,
+  store: Store,
+  render: (h) => h(App),
+  async mounted () {
+    // console.log('this.$route.path => ', this.$route)
+    // console.log('Store.getters.isLogged => ', Store.getters.isLogged)
+
+    // await Repository.Auth.isLogged()
+  }
+}).$mount('#app')
