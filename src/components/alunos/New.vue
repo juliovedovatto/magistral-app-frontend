73790668def33d11cb568493397aeec4fafd3e9e
Julio Vedovatto <juliovedovatto@gmail.com>
2019-09-24 01:02:55 -0300

    Aluno: formulário de pré-cadastro adicionado ao criar novo Aluno

--- src/components/alunos/New.vue
+++ src/components/alunos/New.vue
@@ -3,11 +3,24 @@
     <b-row>
       <b-col>
         <b-button variant="primary" :to="{ name: 'alunos' }">Voltar para Listagem</b-button>
+        <b-button variant="light" @click.prevent="newRecord">Novo Cadastro</b-button>
+      </b-col>
+    </b-row>
+    <b-row v-if="this.notSelected">
+      <b-col>
+        <b-form-group label="Tipo de Cadastro" label-for="input-tipo">
+          <b-select id="input-tipo" v-model.number="tipo" :options="tipos">
+            <template v-slot:first>
+              <option :value="null" disabled>-- Selecione Tipo --</option>
+            </template>
+          </b-select>
+        </b-form-group>
       </b-col>
     </b-row>
     <b-row>
       <b-col>
-        <Form :aluno="aluno" newRecord="true" v-on:form:save="save" />
+        <Form :aluno="aluno" newRecord="true" v-on:form:save="save" v-if="isDetailed" />
+        <FormSimple :aluno="aluno" newRecord="true" v-on:form:save="save" v-else-if="isSimple" />
       </b-col>
     </b-row>
   </b-container>
@@ -15,21 +28,55 @@
 
 <script lang="ts">
 import { Component, Vue, Prop, Emit } from 'vue-property-decorator'
+import objectFilter from '@/utils/object-filter'
+
 import Repository from '@/repository'
 import Aluno from '@/models/Aluno'
+import { TipoCadastro, TipoCadastroLabels } from '@/enums/Aluno'
+
 import Form from './Form.vue'
+import FormSimple from './FormSimple.vue'
+
 
 @Component({
   components: {
-    Form
+    Form,
+    FormSimple
   }
 })
 export default class extends Vue {
   private aluno: Aluno = new Aluno()
+  private tipo: number = -1
+
+  get notSelected (): boolean {
+    return this.tipo === -1
+  }
+
+  get isDetailed (): boolean {
+    return this.tipo === TipoCadastro.TIPO_CADASTRADO
+  }
+
+  get isSimple (): boolean {
+    return this.tipo === TipoCadastro.PRE_CADASTRO
+  }
+
+  get tipos () {
+    return objectFilter(TipoCadastroLabels, (value: string, tipo: number) => Number(tipo) !== TipoCadastro.TIPO_CANCELADO)
+  }
+
+  private newRecord (event: Event) {
+    this.tipo = -1
+  }
 
   @Emit('form:save')
   async save () {
+    if (!TipoCadastro[this.tipo]) {
+      throw new Error('Tipo inválido')
+    }
+
+    this.aluno.tipo_cadastro = this.tipo
     await Repository.Alunos.create(this.aluno)
+
     await this.$router.push({ name: 'alunos' })
   }
 }
---
+++ src/utils/object-filter.ts
@@ -0,0 +1,6 @@
+import GenericObject from '@/types/GenericObject'
+
+export default function objectFilter (obj: GenericObject, predicate: Function): GenericObject {
+  return Object.keys(obj).filter(key => predicate(obj[key], key))
+    .reduce((res: GenericObject, key) => (res[key] = obj[key], res), {})
+}
