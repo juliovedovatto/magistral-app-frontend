c0e708eb1c4dbed8c3436c702ac4f2ee370f9f0c
Julio Vedovatto <juliovedovatto@gmail.com>
2019-09-24 22:56:05 -0300

    Melhoria código plugin de gerenciamento de datas (dayjs)

--- src/plugins/date.ts
+++ src/plugins/date.ts
@@ -1,10 +1,29 @@
 import 'dayjs/locale/pt-br'
 
-import Vue from 'vue'
+import { PluginObject, VueConstructor } from 'vue'
+
 import dayjs from 'dayjs'
 
-export default function VueDate (instance: typeof Vue, options?: any): void {
+declare module 'vue/types/vue' {
+  export interface Vue {
+    $date: dayjs.Dayjs
+  }
+}
+
+function install (instance: VueConstructor, options?: any): void {
   dayjs.locale('pt-br')
 
-  instance.prototype.$date = dayjs
+  Object.defineProperties(instance.prototype, {
+    $date: {
+      get () {
+        return dayjs
+      }
+    }
+  })
 }
+
+export const plugin: PluginObject<any> = {
+  install
+}
+
+export default plugin
