64e2aa57607551152030670d0a7a5c16e350fcc2
Julio Vedovatto <juliovedovatto@gmail.com>
2019-09-28 20:16:25 -0300

    Correções estrutura de layout e menu adicionados

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
-  <b-container id="app">
-    <header>
-    </header>
-    <main>
-      <router-view/>
-    </main>
-    <footer>
-      <b-row>
-        <b-col class="text-right">
-        </b-col>
-      </b-row>
-    </footer>
+  <b-container fluid id="app">
+    <b-row tag="header" no-gutters>
+      <b-col >
+        <Menu />
+      </b-col>
+    </b-row>
+    <b-row tag="main" no-gutters>
+      <b-container>
+        <router-view/>
+      </b-container>
+    </b-row>
+    <b-row tag="footer" no-gutters>
+      <b-col class="text-right">
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+import Menu from '@/components/Menu.vue'
+
+@Component({
+  components: {
+    Menu
+  }
+})
+export default class App extends Vue {
+
+}
+</script>
+
 <style lang="scss">
+#app {
+  &.container-fluid {
+    padding: 0;
+  }
+
+  > header {
+
+  }
+  > main {
+    padding: 20px 0;
+  }
+  > footer {
+
+  }
+}
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,38 @@
+<template functional>
+  <b-navbar toggleable="lg" variant="light">
+    <b-navbar-brand :to="{ name: 'home' }">Cia. Magistral</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item :to="{ name: 'alunos' }">Alunos</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+         <b-nav-item :to="{ name: 'usuarios' }">Usuários</b-nav-item>
+
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item :to="{ name: 'profile' }">Meus Dados</b-dropdown-item>
+          <b-dropdown-item :to="{ name: 'logout' }">Logout</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+
+@Component
+export default class Menu extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
