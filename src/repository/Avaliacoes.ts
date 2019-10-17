7723e11bddc06656f4550869fe72df8bfd148fb5
Julio Vedovatto <juliovedovatto@gmail.com>
2019-10-17 00:49:25 -0300

    API: correção classe repository de avaliações

--- src/repository/Avaliacoes.ts
+++ src/repository/Avaliacoes.ts
@@ -4,32 +4,34 @@ import { AlunoAvaliacao as AlunoAvaliacaoRepositoryType } from '@/types/Reposito
 import Repository from './Repository'
 import UsuarioInfo from '@/models/UsuarioInfo'
 
-export default class AvaliacoesRepository {
-  private resource = '/avaliacoes'
+const resource = '/avaliacoes'
 
-  async find (id: number): Promise<AlunoAvaliacao> {
-    const { data } = await Repository.get(`${this.resource}/${id}`)
+export default class AvaliacoesRepository {
+
+
+  static async find (id: number): Promise<AlunoAvaliacao> {
+    const { data } = await Repository.get(`${resource}/${id}`)
 
     return this.createModel(data)
   }
 
-  async getAll (): Promise<AlunoAvaliacao[]> {
-    const { data } = await Repository.get(this.resource)
+  static async getAll (): Promise<AlunoAvaliacao[]> {
+    const { data } = await Repository.get(resource)
 
     const result = data.map((item: AlunoAvaliacaoRepositoryType) => this.createModel(item))
 
     return result
   }
 
-  async create (AlunoAvaliacao: AlunoAvaliacao): Promise<AlunoAvaliacao> {
-    const { data } = await Repository.post(this.resource, AlunoAvaliacao)
+  static async create (AlunoAvaliacao: AlunoAvaliacao): Promise<AlunoAvaliacao> {
+    const { data } = await Repository.post(resource, AlunoAvaliacao)
 
     const result = this.createModel(data)
 
     return result
   }
 
-  async update (AlunoAvaliacao: AlunoAvaliacao, id: number | null): Promise<boolean> {
+  static async update (AlunoAvaliacao: AlunoAvaliacao, id: number | null): Promise<boolean> {
     let result = true
 
     try {
@@ -37,7 +39,7 @@ export default class AvaliacoesRepository {
         throw new Error('invalid id to update')
       }
 
-      await Repository.put(`${this.resource}/${id}`, AlunoAvaliacao)
+      await Repository.put(`${resource}/${id}`, AlunoAvaliacao)
     } catch (err) {
       result = false
     }
@@ -45,11 +47,11 @@ export default class AvaliacoesRepository {
     return result
   }
 
-  async delete (id: number, aluno: number): Promise<boolean> {
+  static async delete (id: number, aluno: number): Promise<boolean> {
     let result = true
 
     try {
-      const response = await Repository.delete(`${this.resource}/${id}`, { id, aluno })
+      const response = await Repository.delete(`${resource}/${id}`, { id, aluno })
     } catch (err) {
       result = false
     }
@@ -57,7 +59,7 @@ export default class AvaliacoesRepository {
     return result
   }
 
-  private createModel (item: AlunoAvaliacaoRepositoryType): AlunoAvaliacao {
+  private static createModel (item: AlunoAvaliacaoRepositoryType): AlunoAvaliacao {
     const model = new AlunoAvaliacao()
 
     model.id = item.id
