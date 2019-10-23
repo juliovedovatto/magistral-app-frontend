0688526a44d08d24f2ef05015c1b6dc8da946ed7
Julio Vedovatto <juliovedovatto@gmail.com>
2019-10-23 00:31:46 -0300

    Componente de dados básicos do Aluno

---
+++ src/components/common/AlunoInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="aluno">
+    <h2>{{ aluno.nome }}</h2>
+    <p>
+      {{ aluno.cidade }} / {{ aluno.uf }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import Aluno from '@/models/Aluno'
+
+@Component
+export default class CommonEditor extends Vue {
+  @Prop({ required: true })
+  private aluno!: Aluno
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
